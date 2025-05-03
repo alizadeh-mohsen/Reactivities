aab97d8c0bd69659dbcb67790beaac33aadbeb16
Mohsen Alizadeh <kelarens@gmail.com>
2025-05-03 05:52:39 +0100

    Create the Walking Skeleton of the .NET API

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(
+
+    options => options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 //app.UseAuthorization();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        await context.Database.MigrateAsync();
+        await DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
 
 app.MapControllers();
 
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Persistence
+{
+    public class AppDbContext :DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Activity>()
+        //        .Property(a => a.Id)
+        //        .ValueGeneratedOnAdd();
+        //}
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
