d5aaeb1207738f113986b9c3f11f399167ef8a2b
Mohsen Alizadeh <kelarens@gmail.com>
2025-06-30 10:13:14 +0100

    Updated About page

--- client/src/features/about/About.tsx
+++ client/src/features/about/About.tsx
@@ -1,29 +1,54 @@
-import { Box, List, ListItem, ListItemIcon, Paper } from "@mui/material";
+import { Avatar, Box, Grid2, List, ListItem, ListItemIcon, Paper, Stack, Typography } from "@mui/material";
 import StarIcon from '@mui/icons-material/Star';
+
 export default function About() {
     return (
-        <Box alignItems='center' width='80%' mx='auto' >
-            <Paper >
-                <List>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>.Net 9.0, React 19.0 with Typescript</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Web API .Net Core</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Clean Architecture using the CQRS + Mediator pattern</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Entity Framework Core and SQL Server</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>AutoMapper and MediatR</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Identity and Authentication using .Net Core Identity</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Fluent Validation, Middleware and Interceptors </ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>SignalR for chat and comment</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Vite, Tanstack React Query and Axios</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>React Router</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>React Hook Form, Zod, Leaflet and LocationIQ to dispaly map</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>MobX as a clien side state management library</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Material UI</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Cursor pagination, sorting and filtering</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Cloudinary for Image upload</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Photo Upload widget with React dropzone</ListItem>
-                    <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>GitHub actions and workflow for CI/CD and Azure publish</ListItem>
-                </List>
-            </Paper>
-        </Box>
+        <Grid2 container spacing={3}>
+            <Grid2 size={8}>
+                <Paper elevation={3} sx={{ p: 4, borderRadius: 3 }}>
+                    <List>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>.Net 9.0, React 19.0 with Typescript</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Web API .Net Core</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Clean Architecture using the CQRS + Mediator pattern</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Entity Framework Core and SQL Server</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>AutoMapper and MediatR</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Identity and Authentication using .Net Core Identity</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Fluent Validation, Middleware and Interceptors </ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>SignalR for chat and comment</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Vite, Tanstack React Query and Axios</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>React Router</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>React Hook Form, Zod, Leaflet and LocationIQ to dispaly map</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>MobX as a clien side state management library</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Material UI</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Cursor pagination, sorting and filtering</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Cloudinary for Image upload</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Photo Upload widget with React dropzone</ListItem>
+                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>GitHub actions and workflow for CI/CD and Azure publish</ListItem>
+                    </List>
+                </Paper>
+
+            </Grid2>
+            <Grid2  size={4}
+                sx={{
+                    position: 'sticky',
+                    top: 112,
+                    alignSelf: 'flex-start'
+                }} >
+                <Paper elevation={3} sx={{ p: 4, borderRadius: 3 }}>
+
+                    <Stack direction='row' spacing={3} alignItems='center'>
+                        <Avatar
+                            src={`/images/Mohsen.jpg`}
+                            alt='current user image'
+                            sx={{ width: 150, height: 150 }}
+                        />
+                        <Box display='flex' flexDirection='column' gap={2}>
+                            <Typography variant="h6">Built by</Typography>
+                            <a href="https://www.linkedin.com/in/mo-aliz/"><Typography variant="h6" > Mohsen Alizadeh</Typography></a>
+                        </Box>
+                    </Stack>
+                </Paper>
+            </Grid2>
+        </Grid2>
     )
 }
--- client/src/features/account/LoginForm.tsx
+++ client/src/features/account/LoginForm.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { useAccount } from "../../lib/hooks/useAccount"
 import { loginSchema, LoginSchema } from "../../lib/schemas/loginSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Box, Button, Paper, Typography } from "@mui/material";
+import { Box, Button, Chip, Paper, Typography } from "@mui/material";
 import { LockOpen } from "@mui/icons-material";
 import TextInput from "../../app/shared/components/TextInput";
 import { Link, useLocation, useNavigate } from "react-router";
@@ -25,40 +25,47 @@ export default function LoginForm() {
     }
 
     return (
-        <Paper 
-            component='form' 
-            onSubmit={handleSubmit(onSubmit)}
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                p: 3,
-                gap: 3,
-                maxWidth: 'md',
-                mx: 'auto',
-                borderRadius: 3
-            }}
-        >
-            <Box display='flex' alignItems='center' justifyContent='center' 
-                gap={3} color='secondary.main'>
-                <LockOpen fontSize="large" />
-                <Typography variant="h4">Sign in</Typography>
-            </Box>
-            <TextInput label='Email' control={control} name='email' />
-            <TextInput label='Password' type='password' control={control} name='password' />
-            <Button 
-                type='submit' 
-                disabled={!isValid || isSubmitting}
-                variant="contained"
-                size="large"
+        <>
+            <Paper
+                component='form'
+                onSubmit={handleSubmit(onSubmit)}
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    p: 3,
+                    gap: 3,
+                    maxWidth: 'md',
+                    mx: 'auto',
+                    borderRadius: 3
+                }}
             >
-                Login
-            </Button>
-            <Typography sx={{textAlign: 'center'}}>
-                Don't have an account? 
-                <Typography sx={{ml: 2}} component={Link} to='/register' color="primary">
-                    Sign up
+                <Box display='flex' alignItems='center' justifyContent='center'
+                    gap={3} color='secondary.main'>
+                    <LockOpen fontSize="large" />
+                    <Typography variant="h4">Sign in</Typography>
+                </Box>
+                <TextInput label='Email' control={control} name='email' />
+                <TextInput label='Password' type='password' control={control} name='password' />
+                <Button
+                    type='submit'
+                    disabled={!isValid || isSubmitting}
+                    variant="contained"
+                    size="large"
+                >
+                    Login
+                </Button>
+                <Typography sx={{ textAlign: 'center' }}>
+                    Don't have an account?
+                    <Typography sx={{ ml: 2 }} component={Link} to='/register' color="primary">
+                        Sign up
+                    </Typography>
                 </Typography>
-            </Typography>
-        </Paper>
+
+
+            </Paper>
+            <Box sx={{mt:2}} display='flex' alignItems='center' justifyContent='center'>
+                <Chip size="small" label=' or simply login as guest using:  email: guest@test.com, password: Guest@1'
+                    color="secondary" variant="outlined" /></Box>
+        </>
     )
 }
