91ac47e05983b8bc470430800e543a413a144ef6
Mohsen Alizadeh <kelarens@gmail.com>
2025-07-08 14:08:49 +0100

    Changed the About page style

--- client/src/features/about/About.tsx
+++ client/src/features/about/About.tsx
@@ -5,55 +5,76 @@ export default function About() {
     return (
         <Grid2 container spacing={3}>
             <Grid2 size={8}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                    <Paper elevation={3} sx={{ p: 4, borderRadius: 3 }}>
+                        <Typography variant="h6" sx={{ mb: 2 }}>
+                            Backend Technologies and Features:
+                        </Typography>
+                        <List>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>.Net 9.0</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>ASP.NET Restfull Web API</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Clean Architecture using the CQRS + Mediator pattern</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Entity Framework Core and SQL Server</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>AutoMapper and MediatR</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Identity and Authentication using .Net Core Identity</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Fluent Validation, Middleware and Interceptors </ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>SignalR for chat and comment</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Cloudinary for Image upload</ListItem>
+                        </List>
+                    </Paper>
+                    <Paper elevation={3} sx={{ p: 4, borderRadius: 3 }}>
+                        <Typography variant="h6" sx={{  mb: 2 }}>
+                            Frontend Technologies and Features:
+                        </Typography>
+                        <List>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>React 19.0 with Typescript</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Vite, Tanstack React Query and Axios</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>React Router</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>React Hook Form, Zod, Leaflet and LocationIQ to dispaly map</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>MobX as a clien side state management library</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Material UI</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Cursor pagination, sorting and filtering</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Photo Upload widget with React dropzone</ListItem>
+                        </List>
+                    </Paper>
+                    <Paper elevation={3} sx={{ p: 4, borderRadius: 3 }}>
+
+                        <Typography variant="h6" sx={{ mb: 2 }}>
+                            CI/CD and Deployment:
+                        </Typography>
+                        <List>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Azuer App Service</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Azure SQL Database</ListItem>
+                            <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>GitHub actions and workflow</ListItem>
+                        </List>
+                    </Paper>
+                </Box>
+            </Grid2>
+            <Grid2 size={4} sx={{
+                    position: 'sticky',
+                    top: 112,
+                    alignSelf: 'flex-start'
+                }}>
                 <Paper elevation={3} sx={{ p: 4, borderRadius: 3 }}>
                     <Typography>
                         MeetUp is a fake social event platform where users can register,
                         log in, and build their profiles. Create and discover events,
                         explore event details, join or leave gatherings, view attendee lists,
-                        follow other users, chat in real time, and leave comments to stay connected. It's been developed using:</Typography>
-                    <List>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Azuer App Service and Azure SQL Database</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>.Net 9.0, React 19.0 with Typescript</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>ASP.NET Restfull Web API</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Clean Architecture using the CQRS + Mediator pattern</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Entity Framework Core and SQL Server</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>AutoMapper and MediatR</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Identity and Authentication using .Net Core Identity</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Fluent Validation, Middleware and Interceptors </ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>SignalR for chat and comment</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Vite, Tanstack React Query and Axios</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>React Router</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>React Hook Form, Zod, Leaflet and LocationIQ to dispaly map</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>MobX as a clien side state management library</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Material UI</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Cursor pagination, sorting and filtering</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Cloudinary for Image upload</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>Photo Upload widget with React dropzone</ListItem>
-                        <ListItem><ListItemIcon><StarIcon fontSize='small' sx={{ color: 'goldenrod' }} /></ListItemIcon>GitHub actions and workflow for CI/CD and Azure publish</ListItem>
-                    </List>
-                </Paper>
-
-            </Grid2>
-            <Grid2 size={4}
-                sx={{
-                    position: 'sticky',
-                    top: 112,
-                    alignSelf: 'flex-start'
-                }} >
-                <Paper elevation={3} sx={{ p: 4, borderRadius: 3 }}>
-
-                    <Stack direction='row' spacing={3} alignItems='center'>
-                        <Avatar
-                            src={`/images/Mohsen.jpg`}
-                            alt='current user image'
-                            sx={{ width: 150, height: 150 }}
-                        />
-                        <Box display='flex' flexDirection='column' gap={2} sx={{ overflow: 'hidden' }}>
-                            <Typography >Mohsen Alizadeh</Typography>
-                            <Typography >Mobile: (+44) 7932742686</Typography>
-                            <Typography >LinkedIn: <a href="https://www.linkedin.com/in/mo-aliz/">linkedin.com/in/mo-aliz</a></Typography>
-                        </Box>
-                    </Stack>
+                        follow other users, chat in real time, and leave comments to stay connected.</Typography>
+                    <Paper elevation={3} sx={{ mt: 3, p: 4, borderRadius: 3 }}>
+                        <Stack direction='row' spacing={3} alignItems='center'>
+                            <Avatar
+                                src={`/images/Mohsen.jpg`}
+                                alt='current user image'
+                                sx={{ width: 150, height: 150 }}
+                            />
+                            <Box display='flex' flexDirection='column' gap={2} sx={{ overflow: 'hidden' }}>
+                                <Typography >Mohsen Alizadeh</Typography>
+                                <Typography >Mobile: (+44) 7932742686</Typography>
+                                <Typography >LinkedIn: <a href="https://www.linkedin.com/in/mo-aliz/">linkedin.com/in/mo-aliz</a></Typography>
+                            </Box>
+                        </Stack>
+                    </Paper>
                 </Paper>
             </Grid2>
         </Grid2>
